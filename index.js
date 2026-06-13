9932ad698f2053476954474aee53b30d8197d58b
ahobyuplmned2-bit <ahobyuplmned2@gmail.com>
2026-06-13 14:28:19 +0300

    Add WhatsApp bot functionality with Express server

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const express = require('express');
 
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+    console.log('QR RECEIVED');
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body === '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('WhatsApp Bot is running'));
+app.listen(port, () => console.log(`Server on ${port}`));
